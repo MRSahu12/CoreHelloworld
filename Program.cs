eee013cf6990984b4c120d66aa4b148e2ab88d19
MRSahu12 <mihirranjansahu12@gmail.com>
2022-07-01 16:04:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoreHelloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Its created in .net core template");
+            Console.ReadLine();
+        }
+    }
+}
